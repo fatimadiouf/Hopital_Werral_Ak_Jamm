15bf464d80f6645354f9dbcab9484c1c28a0faa1
fatimadiouf <140359948+fatimadiouf@users.noreply.github.com>
2023-08-03 18:10:43 +0000

    Add files via upload

---
+++ service.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SERVICE</title>
+</head>
+<body>
+    <h1>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat laboriosam quod, impedit repellendus animi odit quam ea omnis beatae, blanditiis nesciunt eum distinctio? Quaerat praesentium molestiae beatae, reiciendis est aliquam.
+    </h1>
+</body>
+</html>
